fe5281098a53b7cb00ca92d10f4adca651ebd21a
Madhulahari2526 <kmadhulahari@gmail.com>
2026-06-24 09:44:24 -0400

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,21 @@
+a=1
+b=2
+print(a+b)
+
+
+a=25
+b=5
+print("a is divideb by b the remainder is", a%b)
+
+
+
+a=input("enter the value of a :")
+print(type(a))
+
+
+a=int(input("enter the number a is:",))
+b=int (input("enter tne number b is :",))
+print("b is greater then a ",a>b)
+
+
+
